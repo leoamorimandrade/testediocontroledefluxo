3a6d4d7ee68c7f128989125ebdec260e49dc1c31
leoamorimandrade <142458153+leoamorimandrade@users.noreply.github.com>
2023-09-21 20:07:06 -0300

    firts commit projeto controle de fluxo

---
+++ src/Contador.java
@@ -0,0 +1,25 @@
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nestInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nestInt();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+            
+			contar(parametroUm, parametroDois);
+		
+		}catch (? exception) {
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		
+		int contagem = parametroDois - parametroUm;
+		//realizar o for para imprimir os números com base na variável contagem
+	}
+}
